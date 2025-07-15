7a51ac16477cfae373498cd3b08755b801466e73
RolandSpoofy <135081752+RolandSpoofy@users.noreply.github.com>
2025-07-15 13:57:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hiệu ứng cuộn các liên kết trong trang
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
